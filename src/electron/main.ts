42c293fe18ccd16de1d3988731ca0435f7d19495
furqan4545 <furqan.ali9500@gmail.com>
2024-11-07 07:35:47 +0900

    v2.6

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,17 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
+import { isDev } from "./util.js";
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
-  mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  }
 });
